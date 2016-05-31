e037b2dd40b22dffeea9f9789cf8e2738757cec6
Raphaël Gully <raphael.gully@kubity.com>
2016-05-31 14:09:24 +0200

    cleanup

--- common.js
+++ common.js
@@ -1,9 +1,6 @@
 module.exports = [
 	'components/rerewe/layout/GlobalLayout.jsx',
 	'components/ui/text/TextBlock.jsx',
-		// 'components/rerewe/layout/header/Header.jsx',
-		// 'components/rerewe/layout/menu/Menu.jsx',
-
 	'components/ui/grid/Grid.jsx',
 
 	'react',
